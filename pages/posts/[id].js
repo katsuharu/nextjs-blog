a0fc3a9c2acacf415d8c32474910bbff41e66099
sasaki-katsuharu <sasaki-katsuharu@dmm.com>
2021-02-13 11:53:19 +0900

    implement getStaticProps

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,20 @@
-import { getAllPostIds } from "../../lib/posts";
 import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+}
 
 export async function getStaticPaths() {
+  // Return a list of possible value for id
   const paths = getAllPostIds();
   return {
     paths,
@@ -9,6 +22,12 @@ export async function getStaticPaths() {
   };
 }
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export async function getStaticProps({ params }) {
+  // Fetch necessary data for the blog post using params.id
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
